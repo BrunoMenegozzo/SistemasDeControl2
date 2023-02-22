19f62200b5adf5d294afa500f0815a6b634bc72f
Mene <101132664+BrunoMenegozzo@users.noreply.github.com>
2023-02-21 23:54:20 -0300

    Update MotorDC_2_TP3.m

--- Tps Pucheta/Tp3 Pucheta/MotorDC_2_TP3.m
+++ Tps Pucheta/Tp3 Pucheta/MotorDC_2_TP3.m
@@ -1,24 +1,24 @@
+%Motor DC TP3: El codigo comienza en la linea 129.
 %-------------------------------------------------------------------
-%Motor DC DLQR sin integrador del error. No funciona correctamente porque
-%no llega a la referencia.
+%Motor DC DLQR sin integrador del error. No ajusta la referencia negativa correctamente.
 %-------------------------------------------------------------------
 
-% clear all;close all;
+% clear all;%close all;
 % TamanioFuente=12;
 % ii=0;At=1e-5;ref1=-1.57;ref2=1.57;tF=1; 
 % Ts=At;
 % u=12;Tl=0;
 % 
 % %Condiciones iniciales
-% ia(1)=0;w(1)=0;tita(1)=0;
-% X=[ia(1);w(1);tita(1)];
+% ia(1)=0;tita(1)=0;w(1)=0;
+% X=[ia(1);tita(1);w(1)];
 % 
 % 
 % %Caracteristicas del motor
 % Laa=366e-6; J=5e-9;Ra=55.6;B=0;Ki=6.49e-3;Km=6.53e-3;
 % 
 % %Matrices
-% Ac=[-Ra/Laa 0 -Km/Laa; 0 0 1;Ki/J 0 -B/J];
+% Ac=[-Ra/Laa 0 -Km/Laa; 0 0 1 ;Ki/J 0 -B/J];
 % Bc=[1/Laa;0;0];
 % C=[1 0 0];
 % Per=[0;0;-1/J]; %Matriz perturbacion
@@ -41,8 +41,10 @@
 % rango=rank(M);
 % 
 % % %Funcional de costos
-%  Q=diag([1e-3 1500000 2000]);R=100; %At=1e-5
-% %Q=diag([1 10 50]);R=1; %At=1e-4
+%  %
+%  Q=diag([1 150000 10]);R=3.2e2; %Ts=1e-5 / Con C es [1 0 0].
+%  %Q=diag([1e4 5e8 1e4]);R=8e4; %Ts=1e-4/ Con C es [0 1 0].
+%  %Q=diag([1 10 50]);R=1; %Ts=1e-4
 % H=[A+B*inv(R)*B'*inv(A')*Q -B*inv(R)*B'*inv(A') ; -inv(A')*Q inv(A')];
 % 
 % [V,D]=eig(H);MX1X2=[];
@@ -92,8 +94,8 @@
 % 
 %  %Grafico sin observador
 %  x1(ii)=X(1); %Ia
-%  x2(ii)=X(2); %w
-%  x3(ii)=X(3); %tita
+%  x2(ii)=X(2); %tita
+%  x3(ii)=X(3); %w
 % 
 %  torque(ii)=Tl;%Torque Tl
 %  acc(ii)=u;
@@ -105,9 +107,9 @@
 % subplot(4,1,1);hold on;
 % plot(t,x1(1:numel(t)),color);title('Ia');xlabel('Tiempo [Seg.]');hold on;
 % subplot(4,1,2);hold on;
-% plot(t,x2(1:numel(t)),color);title('\omega_t');xlabel('Tiempo [Seg.]');
+% plot(t,x2(1:numel(t)),color);title('tita');xlabel('Tiempo [Seg.]');
 % subplot(4,1,3);hold on;
-% plot(t,x3(1:numel(t)),color);title('tita');xlabel('Tiempo [Seg.]');
+% plot(t,x3(1:numel(t)),color);title('w');xlabel('Tiempo [Seg.]');
 % 
 % subplot(4,2,7);hold on;
 % plot(t,torque(1:numel(t)),'r');title('Torque de carga');
@@ -117,23 +119,23 @@
 % xlabel('Tiempo [Seg.]');
 % 
 % figure(2);
-% semilogy(t,Mat_J(1:numel(t)),color);grid on;title('Modelo no lineal','FontSize',TamanioFuente);
+% semilogy(t,Mat_J(1:numel(t)),color);grid on;title('Modelo lineal','FontSize',TamanioFuente);
 % xlabel('Tiempo en Seg.','FontSize',TamanioFuente);ylabel('Funcionales J y V','FontSize',TamanioFuente);hold on;
 % %semilogy(t,Jmin*ones(size(Mat_J)),color);
 % semilogy(t,V_L(1:numel(t)),'g');
 
 % % %-----------------------------------------------------------------------------
-% Motor DC DLQR con integrador del error.
+% Motor DC DLQR con integrador del error. Funciona
 % %---------------------------------------------------------------------------
 clear all;close all;
 TamanioFuente=12;
-ii=0;At=1e-5;ref=-1.57;tF=1;
+ii=0;At=1e-5;ref=-1.57;tF=.15;
 Ts=At;
 u=12;Tl=0;
 
 %Condiciones iniciales
-ia(1)=0;w(1)=0;tita(1)=0;
-X=[ia(1);w(1);tita(1)];
+ia(1)=0;tita(1)=0;w(1)=0;
+X=[ia(1);tita(1);w(1)];
 
 
 %Caracteristicas del motor
@@ -142,7 +144,7 @@ Laa=366e-6; J=5e-9;Ra=55.6;B=0;Ki=6.49e-3;Km=6.53e-3;
 %Matrices Tiempo Continuo
 Ac=[-Ra/Laa 0 -Km/Laa; 0 0 1;Ki/J 0 -B/J];
 Bc=[1/Laa;0;0];
-C=[0 0 1];
+C=[0 1 0];
 Per=[0;0;-1/J]; %Matriz perturbacion
 
 %Verifico Controlabilidad y Observabilidad
@@ -188,9 +190,8 @@ aut_controlador=abs(eig(Aa-Ba*Ka));
 Gj=-inv(C*inv(A-B*K)*B);
 
 %Simulacion Lineal
-% Jmin=x'*P*x;J=0;V_L=x'*P*x;
-%Jmin=X'*P*X;
-% Mat_J=zeros(tF/At,1);
+V_L=[X;0]'*P*[X;0];
+Jmin=[X;0]'*P*[X;0];
 Mat_J=0;
 V_L=[X;0]'*P*[X;0];
 Jl=0;Jmin_L=V_L;
@@ -198,20 +199,23 @@ ve1(1)=0;vei=0;
 for t=0:At:tF
  ii=ii+1;k=ii+2;
  
- Tl_arreglo=[0 ((1.15e-8/2)*sign(sin(10.472*t))+(1.15e-8/2))];
+ Tl_arreglo=[0 ((1.15e-8/2)*sign(sin(10.472*6*t))+(1.15e-8/2))];
  Tl=Tl_arreglo(2);
  
  Y_=C*X;
  e1=ref-Y_(1);
-%  V_Li=[X;ve1(ii)]'*P*[X;ve1(ii)];
-%  V_L=[V_L V_Li];
+ 
  vei=vei+e1;
  ve1(ii)=vei;
+
   u=-Ka*[X;ve1(ii)];%Estado ampliado
  
-%  Jl=Jl+[X;ve1(ii)]'*Q*[X;ve1(ii)]+u'*R*u';
-%  Mat_J=[Mat_J Jl];
-%  V_L=[V_L X'*P*X];
+  %Lyapunov
+ V_Li=[X;ve1(ii)]'*P*[X;ve1(ii)];
+ V_L=[V_L V_Li];
+ Jl=Jl+[X;ve1(ii)]'*Q*[X;ve1(ii)]+u'*R*u';
+ Mat_J=[Mat_J Jl];
+
  
  X=A*X+B*u+Per*Tl;
 
@@ -231,8 +235,8 @@ for t=0:At:tF
  color='r';
     %Grafico sin observador
      x1(ii)=X(1); %Ia
-     x2(ii)=X(2); %w
-     x3(ii)=X(3); %tita
+     x2(ii)=X(2); %tita
+     x3(ii)=X(3); %w
  
  torque(ii)=Tl;%Torque Tl
  acc(ii)=u;
@@ -244,9 +248,9 @@ figure(1);
 subplot(4,1,1);hold on;
 plot(t,x1(1:numel(t)),color);title('Ia');xlabel('Tiempo [Seg.]');hold on;
 subplot(4,1,2);hold on;
-plot(t,x2(1:numel(t)),color);title('\omega_t');xlabel('Tiempo [Seg.]');
+plot(t,x2(1:numel(t)),color);title('tita');xlabel('Tiempo [Seg.]');
 subplot(4,1,3);hold on;
-plot(t,x3(1:numel(t)),color);title('tita');xlabel('Tiempo [Seg.]');
+plot(t,x3(1:numel(t)),color);title('omega');xlabel('Tiempo [Seg.]');
 
 subplot(4,2,7);hold on;
 plot(t,torque(1:numel(t)),'r');title('Torque de carga');
@@ -255,9 +259,8 @@ subplot(4,2,8);hold on;
 plot(t,acc(1:numel(t)),color);title('Accion de control');
 xlabel('Tiempo [Seg.]');
 
-% figure(2);
-% semilogy(t,Mat_J(1:numel(t)),color);grid on;title('Modelo no lineal','FontSize',TamanioFuente);
-% xlabel('Tiempo en Seg.','FontSize',TamanioFuente);ylabel('Funcionales J y V','FontSize',TamanioFuente);hold on;
-% %semilogy(t,Jmin*ones(size(Mat_J)),color);
-% semilogy(t,V_L(1:numel(t)),'g');
-
+figure(2);
+semilogy(t,Mat_J(1:numel(t)),color);grid on;title('Modelo no lineal','FontSize',TamanioFuente);
+xlabel('Tiempo en Seg.','FontSize',TamanioFuente);ylabel('Funcionales J y V','FontSize',TamanioFuente);hold on;
+%semilogy(t,Jmin*ones(size(Mat_J)),color);
+semilogy(t,V_L(1:numel(t)),'g');
